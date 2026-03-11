89d163d85af8eb948faac6d14dbce3494405c615
Xmaji <xmaji@gmail.com>
2026-03-11 11:53:23 +0100

    fix auth security

--- src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
+++ src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
@@ -3,29 +3,28 @@ package com.fantasylol.fantasy_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
 
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // ENDPOINTS PUBLICOS
+                .requestMatchers("/auth/**").permitAll()
 
-                .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers("/auth/**").permitAll()
-
-                        .anyRequest().authenticated()
-                );
+                // TODO LO DEMÁS REQUIERE LOGIN
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
 
         return http.build();
     }
