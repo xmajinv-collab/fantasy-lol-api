d98fb3546d1d71663c3f39d982afcd5beda3c2b8
Xmaji <xmaji@gmail.com>
2026-03-12 10:47:32 +0100

    fix security auth endpoints

--- src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
+++ src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
@@ -2,47 +2,24 @@ package com.fantasylol.fantasy_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            // API REST → sin CSRF
             .csrf(csrf -> csrf.disable())
-
-            // permitir CORS para React
-            .cors(cors -> {})
-
-            // configuración de endpoints
             .authorizeHttpRequests(auth -> auth
-
-                    // auth libre
-                    .requestMatchers("/api/auth/**").permitAll()
-
-                    // swagger si lo usas
-                    .requestMatchers(
-                            "/swagger-ui/**",
-                            "/v3/api-docs/**"
-                    ).permitAll()
-
-                    // todo lo demás requiere login
-                    .anyRequest().authenticated()
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
             )
-
-            // API REST sin sesiones
             .sessionManagement(session ->
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             );
 
         return http.build();
