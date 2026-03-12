b7274095c03eb52b9a58f28c115dca16823cf93d
Xmaji <xmaji@gmail.com>
2026-03-12 12:01:06 +0100

    fix api upgrade

--- src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
+++ src/main/java/com/fantasylol/fantasy_api/config/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.fantasylol.fantasy_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,17 +12,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .cors(cors -> {}) // ACTIVA CORS
             .csrf(csrf -> csrf.disable())
-
+            .cors(cors -> {})
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .anyRequest().authenticated()
-            )
-
-            .sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .anyRequest().permitAll()
             );
 
         return http.build();
